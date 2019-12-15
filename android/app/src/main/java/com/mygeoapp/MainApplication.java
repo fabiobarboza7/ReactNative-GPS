c7ea5bf8c9536215b24cc02d49fe60e028487506
Fábio Souza <fabiobarboza7@hotmail.com>
2019-12-15 17:17:37 -0300

    mapbox android working

--- android/app/src/main/java/com/mygeoapp/MainApplication.java
+++ android/app/src/main/java/com/mygeoapp/MainApplication.java
@@ -4,12 +4,12 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.mapbox.rctmgl.RCTMGLPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.mapbox.rctmgl.RCTMGLPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -24,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          new RCTMGLPackage()
+          new RCTMGLPackage();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,88 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Alert, AppState } from 'react-native';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+import Geolocation from '@react-native-community/geolocation';
+// import { Container } from './styles';
 
-import { Container } from './styles';
-import CurrentPosition from '../CurrentPosition';
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoiZmFiaW9iYXJib3phNyIsImEiOiJjandjNm1yZWwwYWprNDlvN3hmNXltdnVyIn0.0oAJ8Ro030AnmxkKcCZPxg'
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  annotationContainer: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 15,
+  },
+  annotationFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: 'purple',
+    transform: [{ scale: 0.8 }],
+  },
+});
 
 export default function Main() {
+  const [error, setError] = useState('');
+  const [position, setPosition] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  const getPosition = () => {
+    Geolocation.getCurrentPosition(
+      pos => {
+        setError('');
+        setPosition({
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+        });
+      },
+      e => {
+        if (e.PERMISSION_DENIED === 1) {
+          Alert.alert('Habilite o GPS e reinicie o APP');
+        }
+        setError(e.message);
+      }
+    );
+  };
+
+  function renderAnnotations() {
+    return (
+      <MapboxGL.PointAnnotation
+        id="myGeoApp"
+        coordinate={[position.longitude, position.latitude]}
+      >
+        <View style={styles.annotationContainer}>
+          <View style={styles.annotationFill} />
+        </View>
+        <MapboxGL.Callout title="Localização do Canalha" />
+      </MapboxGL.PointAnnotation>
+    );
+  }
+
+  useEffect(() => {
+    getPosition();
+  }, []);
+
   return (
-    <Container>
-      <CurrentPosition />
-    </Container>
+    <MapboxGL.MapView
+      styleURL={MapboxGL.StyleURL.Dark}
+      style={styles.container}
+    >
+      <MapboxGL.Camera
+        zoomLevel={13}
+        centerCoordinate={[position.longitude, position.latitude]}
+      />
+      {renderAnnotations()}
+    </MapboxGL.MapView>
   );
 }
 
